fa193b0e0ec10f99b33fa495387b6a301caaecb3
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-19 11:17:40 +0300

    add socket.io server&client connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import {io} from "socket.io-client";
 import "./App.css";
+ 
+
+
+const socket= io("http://localhost:3001");
 
 function App() {
-  return <>Hello</>;
+  const [message, setMessage] = useState('');
+  const [receivedMessage, setReceivedMessage] = useState('');
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setReceivedMessage(data.message)
+    })
+  }, [socket])
+
+  const sendMessage = () => {
+    socket.emit("send_message", {message});
+  }
+
+  return (
+    <>
+      <input type="text" placeholder="message..." onChange={(e) => setMessage(e.target.value)}/>
+      <button onClick={sendMessage}>Send Message</button>
+
+      <h2>Coming Message from another users</h2>
+      <p>{receivedMessage}</p>
+    </>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import { Server } from "socket.io";
 import cors from "cors";
 
+
 const app = express();
 app.use(cors());
 
@@ -11,10 +12,19 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
-    methods: ['GET', 'POST']
-  }
+    methods: ["GET", "POST"],
+  },
 });
 
-server.listen(3001, () => {
-  console.log("server is running")
+io.on("connection", (socket) => {
+  // console.log(`user connected ${socket.id}`)
+
+  socket.on("send_message", (data) => {
+    // console.log(data.message)
+    socket.broadcast.emit("receive_message", data)
+  })
 })
+
+server.listen(3001, () => {
+  console.log("server is running");
+});
